958d71ecd69382dcb137d6adc7337156a9395497
Kumaraguhan <147791603+kumaraguhan@users.noreply.github.com>
2025-07-30 15:55:22 +0530

    Create List view for Topics and Forms for Adding new Topics (#2)
    Here the List view , url Routing, and Templates for Topics were added, and also Form for Adding new Topic is added using ModelForm and Django Form API

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'boards'
+    'boards',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
