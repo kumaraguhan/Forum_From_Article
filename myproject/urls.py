ddc1df85ebef2c14f84c94eb71e9cfc88d23686a
Kumaraguhan <147791603+kumaraguhan@users.noreply.github.com>
2025-07-31 17:23:22 +0530

    Create List View for Posts for a Topic and Use Django ORM (#4)

--- myproject/urls.py
+++ myproject/urls.py
@@ -32,4 +32,6 @@ urlpatterns = [
 
     path('settings/password/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'), name='password_change'),
     path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/', views.topic_posts, name='topic_posts'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
 ]
